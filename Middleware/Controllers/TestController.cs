e84eb1ebcebacd70e327f30c3918eb9f8a16bc73
abdulkadirmusuk <58270978+abdulkadirmusuk@users.noreply.github.com>
2021-09-11 01:07:44 +0300

    Add project files.

---
+++ Middleware/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Middleware.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpPost("TestAuthentication")]
+        public IActionResult TestAuthentication()
+        {
+            var status = HttpContext.Response.StatusCode;
+            if (status == (int) HttpStatusCode.Unauthorized)
+            {
+                return Unauthorized("Yetkiniz yok");
+            }
+            return Ok("Successful!");
+        }
+    }
+}
---
+++ Middleware/CustomMiddlewares/AuthenticationMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middleware.CustomMiddlewares
+{
+    public class AuthenticationMiddleware
+    {
+        //bir sonraki delege ye geç demek için request delege eklenir
+
+        private readonly RequestDelegate _next;
+        public AuthenticationMiddleware(RequestDelegate next)
+        {
+            _next = next; //bir sonraki delege ye geç
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            string authHeader = httpContext.Request.Headers["Authorization"];
+            //basic kadir:12345 şeklinde data gelecek
+            //authToken varsa ve basic ile başlıyorsa
+            if (authHeader != null && authHeader.StartsWith("basic", System.StringComparison.OrdinalIgnoreCase))
+            {
+                var token = authHeader.Substring(6).Trim();
+                //base64 convertion
+                var credentialString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
+                var credentials = credentialString.Split(':');
+                if (credentials[0] == "kadir" && credentials[1] == "12345")
+                {
+                    //identity principle create
+                    var claims = new[]
+                    {
+                        new Claim("name", credentials[0]),
+                        new Claim(ClaimTypes.Role, "Admin") //admin rolu ataması
+                    };
+                    var identity = new ClaimsIdentity(claims, "Basic");
+                    httpContext.User = new ClaimsPrincipal(identity);
+                }
+
+            }
+            else httpContext.Response.StatusCode = (int) HttpStatusCode.Unauthorized;
+            await _next(httpContext); //ilgili context sonraki middleware e aktarılır.
+        }
+    }
+}
